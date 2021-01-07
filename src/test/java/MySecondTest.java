880ff31a94105fa53ddcc8f81ae2b816dfaf6954
jaya <jayaseyyadri@gmail.com>
2021-01-07 11:41:27 -0600

    completed working on testing

---
+++ src/test/java/MySecondTest.java
@@ -0,0 +1,44 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MySecondTest {
+
+@Test
+public void checkString(){
+    String name ="Codeup";
+    String name2 ="CodeUp";
+
+    assertEquals(name,name2);
+}
+
+@Test
+    public void objectsNotSame(){
+    List<String> languages = new ArrayList<>();
+    List<String> moreLanguages = new ArrayList<>();
+        assertNotSame(languages,moreLanguages);
+}
+@Test
+    public void arraysEqual(){
+    int[] numbers = {1, 2, 3};
+    int[] otherNumbers = new int[3];
+    otherNumbers[0] = 1;
+    otherNumbers[1] = 2;
+    otherNumbers[2] = 3;
+    assertArrayEquals(otherNumbers,numbers);
+}
+
+@Test
+    public void languages() {
+    String language = "PHP";
+    language.contains("H"); // use assertTrue for this statement
+    language.contains("J"); // use assertFalse for this statement
+
+  assertTrue(language.contains("H"));
+    assertFalse(language.contains("J"));
+
+    }
+}
